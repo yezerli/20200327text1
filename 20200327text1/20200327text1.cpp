c4066354a0f98df20aa74782883897930c9ab769
yezerli <1123603008@qq.com>
2020-03-27 14:44:59 +0800

    1234整合版

--- 20200327text1/20200327text1.cpp
+++ 20200327text1/20200327text1.cpp
@@ -9,15 +9,33 @@ int main()
 {
 	VideoCapture cap;
 	cap.open(0);
-
+	if (!cap.isOpened())
+	{
+		std::cout << "不能打开视频文件" << std::endl;
+		return-1;
+	}
+	double fps = cap.get(CAP_PROP_FPS);
+	std::cout << "fps" << fps << std::endl;
 	while (1)
 	{
 		Mat frame;
 		Mat medianblur_frame;
-		bool rSucess = cap.read(frame);
+		Mat blur_frame;
+		Mat Gaussianblur_frame;
+		Mat dx;
+		Mat dy;
+		cap.read(frame);
 		medianBlur(frame, medianblur_frame, 3);
+		blur(frame, blur_frame,Size (3,3));
+		GaussianBlur(frame, Gaussianblur_frame,Size(3,3),1,0);
+		Sobel(frame, dx, CV_16SC1, 1, 0, 3);
+		Sobel(frame, dy, CV_16SC1, 0, 1, 3);
 		imshow("frame", frame);
 		imshow("medianblur_frame", medianblur_frame);
+		imshow("blur_frame", blur_frame);
+		imshow("Gaussianblur_frame", Gaussianblur_frame);
+		imshow("dx", dx);
+		imshow("dy", dy);
 		waitKey(0);
 	}
 }
