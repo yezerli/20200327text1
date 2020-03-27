b843a6aa562c6b029b687b5a8706fdefb1aad94f
yezerli <1123603008@qq.com>
2020-03-27 15:02:52 +0800

    修改之后较为明显

--- 20200327text1/20200327text1.cpp
+++ 20200327text1/20200327text1.cpp
@@ -24,18 +24,22 @@ int main()
 		Mat Gaussianblur_frame;
 		Mat dx;
 		Mat dy;
+		Mat abs_dx;
+		Mat abs_dy;
 		cap.read(frame);
 		medianBlur(frame, medianblur_frame, 3);
 		blur(frame, blur_frame,Size (3,3));
 		GaussianBlur(frame, Gaussianblur_frame,Size(3,3),1,0);
 		Sobel(frame, dx, CV_16SC1, 1, 0, 3);
+		convertScaleAbs(dx, abs_dx);
 		Sobel(frame, dy, CV_16SC1, 0, 1, 3);
+		convertScaleAbs(dy, abs_dy);
 		imshow("frame", frame);
 		imshow("medianblur_frame", medianblur_frame);
 		imshow("blur_frame", blur_frame);
 		imshow("Gaussianblur_frame", Gaussianblur_frame);
-		imshow("dx", dx);
-		imshow("dy", dy);
+		imshow("dx", abs_dx);
+		imshow("dy", abs_dy);
 		waitKey(0);
 	}
 }
